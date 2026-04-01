2f81c4056899d233f2370b20b15c5a274b0cd3d9
Tran Ho <tranhonguyen.work@gmail.com>
2026-04-01 15:08:42 +0700

    today's weather

---
+++ today_weather.js
@@ -0,0 +1,13 @@
+let temperature = 30;
+let conditions = "sunny";
+let sunriseTime = "6:00 AM";
+let sunsetTime = "5:30 PM";
+
+console.log(`Temperature: ${temperature}`);
+console.log(`Conditions: ${conditions}`);
+console.log(`Sunrise time: ${sunriseTime}`);
+console.log(`Sunset time: ${sunsetTime}`);
+
+console.log(
+  `It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`,
+);
