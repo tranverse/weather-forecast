c27cfe0c9c70a9a441485d1237a7470b2827ef1b
Tran Ho <tranhonguyen.work@gmail.com>
2026-04-01 19:15:38 +0700

    Challenge 2

--- weather_object.js
+++ weather_object.js
@@ -30,3 +30,54 @@ console.log(todaysWeather.astronomy.sunrise);
 console.log(
   `Today is ${todaysWeather.conditions} with a high of ${todaysWeather.temperature.high}°F and a low of ${todaysWeather.temperature.low}°F. Sunrise is at ${todaysWeather.astronomy.sunrise} and sunset is at ${todaysWeather.astronomy.sunset}.`,
 );
+
+// challenge 2
+var weatherForecast = [
+  {
+    day: "Today",
+    temperature: {
+      high: 55,
+      low: 32,
+    },
+    conditions: "sunny",
+    astronomy: {
+      sunrise: "7:43 AM",
+      sunset: "5:09 PM",
+    },
+  },
+  {
+    day: "Saturday",
+    temperature: {
+      high: 50,
+      low: 29,
+    },
+    conditions: "cloudy",
+    astronomy: {
+      sunrise: "7:44 AM",
+      sunset: "5:08 PM",
+    },
+  },
+  {
+    day: "Sunday",
+    temperature: {
+      high: 47,
+      low: 35,
+    },
+    conditions: "chance of rain",
+    astronomy: {
+      sunrise: "7:45 AM",
+      sunset: "5:07 PM",
+    },
+  },
+];
+
+console.log(`Today weather condition: ${weatherForecast[0].conditions}`);
+console.log(
+  `Saturday's high temperature: ${weatherForecast[1].temperature.high}`,
+);
+console.log(`Saturday's sunrise time: ${weatherForecast[1].astronomy.sunrise}`);
+console.log(`Sunday's conditions: ${weatherForecast[2].conditions}`);
+console.log(`Sunday's sunset time: ${weatherForecast[2].astronomy.sunset}`);
+console.log(
+  `The high on ${weatherForecast[2].day} will be ${weatherForecast[2].temperature.high} and the low will be ${weatherForecast[2].temperature.low}. We're predicting a ${weatherForecast[2].conditions}. The sun will rise at ${weatherForecast[2].astronomy.sunrise} and set at ${weatherForecast[2].astronomy.sunset}.`,
+);
