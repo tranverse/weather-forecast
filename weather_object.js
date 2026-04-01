e7dbdbc0dfdea1b6801e27a936250ddbd73cb30c
Tran Ho <tranhonguyen.work@gmail.com>
2026-04-01 17:06:48 +0700

    Weather Object

---
+++ weather_object.js
@@ -0,0 +1,10 @@
+let weatherObject = {
+  temperature: 30,
+  conditions: "Sunny",
+  sunriseTime: "5:00 AM",
+  sunsetTime: "6:00 PM",
+};
+console.log(`Current temperature: ${weatherObject.temperature}`);
+console.log(
+  `It's ${weatherObject.temperature}°C and ${weatherObject.conditions}. Sunrise at ${weatherObject.sunriseTime}, sunset at ${weatherObject.sunsetTime}.`,
+);
