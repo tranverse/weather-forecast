74d879821d011d2b7c1acb1d2e65d7b933997ac7
Tran Ho <tranhonguyen.work@gmail.com>
2026-04-01 17:17:21 +0700

    Challenge 1

--- weather_object.js
+++ weather_object.js
@@ -8,3 +8,25 @@ console.log(`Current temperature: ${weatherObject.temperature}`);
 console.log(
   `It's ${weatherObject.temperature}°C and ${weatherObject.conditions}. Sunrise at ${weatherObject.sunriseTime}, sunset at ${weatherObject.sunsetTime}.`,
 );
+
+// challenge 1
+const todaysWeather = {
+  temperature: {
+    high: 55,
+    low: 32,
+  },
+  conditions: "sunny",
+  astronomy: {
+    sunrise: "7:43 AM",
+    sunset: "5:09 PM",
+  },
+};
+
+console.log(todaysWeather.temperature.high);
+console.log(todaysWeather.temperature.low);
+console.log(todaysWeather.conditions);
+console.log(todaysWeather.astronomy.sunrise);
+
+console.log(
+  `Today is ${todaysWeather.conditions} with a high of ${todaysWeather.temperature.high}°F and a low of ${todaysWeather.temperature.low}°F. Sunrise is at ${todaysWeather.astronomy.sunrise} and sunset is at ${todaysWeather.astronomy.sunset}.`,
+);
